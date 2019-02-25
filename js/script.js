4f14c454475d856fbc27e74a7683cf1476732b92
Harry Northam <harrynortham@icloud.com>
2019-02-25 23:19:00 +0100

    Mobile hamburger menu with Javascript toggle

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*Toggle hamburger menu links*/
+function burgerNavigationToggle() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
