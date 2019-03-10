d9066e66f9791d6277745186352a3e3c3caafc4e
Harry Northam <harrynortham@icloud.com>
2019-03-10 08:51:08 +0100

    Moved JS after dom load

--- js/script.js
+++ js/script.js
@@ -7,3 +7,25 @@ function burgerNavigationToggle() {
     x.style.display = "block";
   }
 }
+
+/*Create a cookie*/
+
+function createCookie(name,value,days) {
+     if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+     }
+     else var expires = "";
+     document.cookie = name+"="+value+expires+";";
+}
+
+
+/*Add a promo popup, hide for X days when clicked*/
+document.getElementById('promoClick').addEventListener("click", promoCookieSet);
+
+function promoCookieSet() {
+    document.getElementById("demo").innerHTML = "Cookie is set";
+    createCookie("hello","there",12);
+    console.log("Button clicked");
+  }
