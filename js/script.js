5034927e5ca021ac95b1191b1d84ebef49665ace
Harry Northam <harrynortham@icloud.com>
2019-03-11 10:50:55 +0100

    Added css transitions to links

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 /*Toggle hamburger menu links*/
+
 function burgerNavigationToggle() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
@@ -22,6 +23,7 @@ function createCookie(name,value,days) {
 
 
 /*Add a promo popup, hide for X days when clicked*/
+
 document.getElementById('promoClick').addEventListener("click", promoCookieSet);
 
 function promoCookieSet() {
